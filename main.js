066f8e4f061512b740e4ca0e7e0d44c8a543c38d
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-08-21 09:36:39 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,66 +1,43 @@
-var SpeechRecognition = window.webkitSpeechRecognition;
-
-var recognition = new SpeechRecognition();
-
-function start()
-{
-document.getElementById("textbox").innerHTML = "";
-recognition.start();
-}
-recognition.onresult = function run (event){
-
-
-    console.log(event)
-
-    var Content = event.results[0][0].transcript;
-    console.log(Content);
-    if(Content =="take my selfie"){
-        console.log("taking selfie ---");
-        speak();
-    }
-    
-
-    document.getElementById("textbox").innerHTML = Content;
-
-}
-
-function speak(){
-    var synth = window.speechSynthesis;
-
-    speak_data = "Taking your Selfie in 5 seconds";
-
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-
-    synth.speak(utterThis);
-    Webcam.attach(camera);
-
-    setTimeout(function()
-    {
-        take_snapshot();
-        save();
-    }, 5000);
-}
-
 Webcam.set({
-    width: 320,
-    height: 240,
+    width: 400,
+    height: 400,
     image_format: 'png',
-    png_quality: 90
-  });
+    png_quality:90 
+ });
+
  camera = document.getElementById("camera");
 
+Webcam.attach(camera);
 
- function take_snapshot()
- {
-     Webcam.snap(function(data_uri) {
-      document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
-     })
- }
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+       document.getElementById("result").innerHTML= '<img id="captured_image"  src="'+data_uri+'"/>';
+    });
+}
 
- function save()
- {
-     link = document.getElementById("link");
-     image = document.getElementById("selfie_image").src ;
-     link.href = image;
-     link.click();
+ console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KxtS-IzSN/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results){
+if (error) {
+    console.error(error);
+ }  else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
  }
+}
