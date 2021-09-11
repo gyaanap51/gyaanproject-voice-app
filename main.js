2b497d6e1a451f40c54f1379aa50ad4adb623f0b
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-09-11 09:16:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,52 @@
-Webcam.set({
-    width: 400,
-    height: 400,
-    image_format: 'png',
-    png_quality:90 
- });
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(640,480);
+    video = createCapture(VIDEO);
+    canvas.position(650,100);
+    video.size(640,480);
 
- camera = document.getElementById("camera");
-
-Webcam.attach(camera);
-
-function take_snapshot()
-{
-    Webcam.snap(function(data_uri) {
-       document.getElementById("result").innerHTML= '<img id="captured_image"  src="'+data_uri+'"/>';
-    });
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+   
 }
 
- console.log('ml5 version:', ml5.version);
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+liftWristX = 0;
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KxtS-IzSN/model.json',modelLoaded);
-
-function modelLoaded()
+function gotPoses(results)
 {
-    console.log('Model Loaded!');
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +"noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX +" rightWristX = "+ rightWristX +"difference = " + difference);
+    }
 }
 
-function check()
-{
-    img = document.getElementById('captured_image');
-    classifier.classify(img, gotResult);
+
+
+
+
+function draw(){
+    background('#2AD5AF');
+
+    document.getElementById("square_side").innerHTML = "Length of a Square will be" + difference + "px";
+     fill('#f59942');
+     stroke('#f55742');
+     textSize(difference);
+     text("Gyaan", noseX, noseY);
 }
-
-
-function gotResult(error, results){
-if (error) {
-    console.error(error);
- }  else {
-    console.log(results);
-    document.getElementById("result_object_name").innerHTML = results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
- }
+function modelLoaded(){
+    console.log("PoseNet Is Initialized!");
 }
